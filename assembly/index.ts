741017f0843fd189bb1504add9fa14e77e41d16f
ysun062 <ysun062@gmail.com>
2022-11-20 03:59:40 +0000

    created greeting contract

---
+++ assembly/index.ts
@@ -0,0 +1,11 @@
+import { storage, Context } from "near-sdk-as";
+
+export class Greeting {
+    getGreeting(accountId: string): string | null {
+        return storage.get<string>(accountId, "Greeting not available");
+    }
+
+    setGreeting(greeting: string): void{
+        storage.set(Context.sender, greeting);
+    }
+}
